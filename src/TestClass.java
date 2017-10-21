5a9a47c9df36ecb8f933f09038bae395977e14cf
EileenChau <eileen_chau@student.wayland.k12.ma.us>
2017-10-20 20:24:23 -0400

    timer and main

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,12 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
 /**
  * Created by student on 9/7/17.
  */
-public class TestClass {
+public class TestClass extends JPanel {
+
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
+    private Timer timer;
+
+    public TestClass(){
+        setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        timer = new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                repaint();
+            }
+        });
+        timer.start();
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("What should we make?");
-        System.out.println("idk -sarah");
-        System.out.println("lol we should code Wolfquest");
+        JFrame window = new JFrame("Something");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        TestClass panel = new TestClass();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
 
 }
