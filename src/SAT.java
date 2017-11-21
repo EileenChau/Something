12895ee4dc1973ebd90949c75c634c518371c5ca
sarahzhang10 <sarah_zhang@student.wayland.k12.ma.us>
2017-11-21 15:12:32 -0500

    sat scaler

---
+++ src/SAT.java
@@ -0,0 +1,17 @@
+/**
+ * Created by student on 11/21/17.
+ */
+public class SAT {
+
+    public SAT(){
+
+    }
+
+    public int scaleEnglish(int english){
+        return 400-english;
+    }
+
+    public int scaleMath(int math){
+        return 400-math;
+    }
+}
--- src/TestClass.java
+++ src/TestClass.java
@@ -2,48 +2,76 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Created by student on 9/7/17.
  */
-public class TestClass extends JPanel {
+public class TestClass{
 
-    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
-    private Timer timer;
-
+//    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
+    private SAT sat;
+    private int eng;
+    private int math;
+//    private Timer timer;
+//
     public TestClass(){
-        setSize(FRAMEWIDTH, FRAMEHEIGHT);
+        sat = new SAT();
 
-        timer = new Timer(40, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                repaint();
-            }
-        });
-        timer.start();
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter Number of Questions Wrong in the Reading Section");
+        int reading = Integer.parseInt(input.next());
+
+        System.out.println("Enter Number of Questions Wrong in the Writing Section");
+        int writing = Integer.parseInt(input.next());
+
+        eng = sat.scaleEnglish(reading) + sat.scaleEnglish(writing);
+
+        System.out.println("Enter Number of Questions Wrong in the Math (non calc) Section");
+        int mathNonCalc = Integer.parseInt(input.next());
+
+        System.out.println("Enter Number of Questions Wrong in the Math (calc) Section");
+        int mathCalc = Integer.parseInt(input.next());
+
+        math = sat.scaleMath(mathNonCalc)+sat.scaleMath(mathCalc);
+
+        System.out.println("English Score: " + eng + " Math Score: " + math);
+
+//        setSize(FRAMEWIDTH, FRAMEHEIGHT);
+//
+//        timer = new Timer(40, new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent actionEvent) {
+//                repaint();
+//            }
+//        });
+//        timer.start();
+//
     }
-
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
-    }
+//
+//    public void paintComponent(Graphics g){
+//        super.paintComponent(g);
+//        Graphics2D g2 = (Graphics2D)g;
+//    }
 
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("Something");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
 
-        TestClass panel = new TestClass();
-        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
 
-        panel.setFocusable(true);
-        panel.grabFocus();
-
-        window.add(panel);
-        window.setVisible(true);
-        window.setResizable(false);
+//        JFrame window = new JFrame("Something");
+//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+//
+//        TestClass panel = new TestClass();
+//        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+//
+//        panel.setFocusable(true);
+//        panel.grabFocus();
+//
+//        window.add(panel);
+//        window.setVisible(true);
+//        window.setResizable(false);
     }
 
 }
